4d7608daf7e1998fc9e55dad98fd29b91db3c745
franco-giordano <31712363+franco-giordano@users.noreply.github.com>
2023-10-14 16:57:41 -0300

    New `solid` Drawer menu

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import { SplashScreen, LandingScreen, PreRegisterScreen, HomeScreen, TakePictureStepScreen, CameraTestScreen, PreRegisterLastInstructionsScreen } from './src/scenes';
 import ApprovedSubjects from './src/scenes/home/subsections/HomeOptions/ApprovedSubjects';
@@ -8,96 +8,121 @@ import PendingSubjects from './src/scenes/home/subsections/HomeOptions/PendingSu
 import DeliverFinalExam from './src/scenes/home/subsections/HomeOptions/DeliverFinalExam';
 import VerifyIdentity from './src/scenes/home/subsections/HomeOptions/VerifyIdentity';
 import FilterNavBarButton from './src/scenes/home/filterNavBarButton';
-import { Provider, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './src/redux/store';
+import { DrawerItemList, DrawerContentScrollView, createDrawerNavigator } from '@react-navigation/drawer';
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const TAB_MENU_SHOWN_SCREENS = [
+  "Home",
+  "PendingSubjects",
+  "ApprovedSubjects",
+  "DeliverFinalExam",
+  "VerifyIdentity"
+]
+
+function CustomDrawerContent(props: any) {
+  const { state, ...rest } = props;
+  const newState = {
+    ...state, routes: state.routes.filter((route: any) => {
+      // Only include the routes you want here
+      return TAB_MENU_SHOWN_SCREENS.includes(route.name);
+    })
+  };
+
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...rest} state={newState} />
+    </DrawerContentScrollView>
+  );
+}
 
 const App = () => {
   return (
     <Provider store={store}>
       <ActionSheetProvider>
         <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName="Splash"
-            screenOptions={{ gestureEnabled: false }}
+          <Drawer.Navigator
+            initialRouteName="Landing"
+            drawerContent={props => <CustomDrawerContent {...props} />}
           >
-            <Stack.Screen
-              name="Splash"
-              component={SplashScreen}
-              options={{ headerShown: false }}
-            />
-
-            <Stack.Screen
-              name="Landing"
-              component={LandingScreen}
-              options={{ headerShown: false, title: 'Inicio' }}
-            />
-
-            <Stack.Screen
-              name="PreRegister"
-              component={PreRegisterScreen}
-              options={{ headerShown: true, title: 'Pre-registro' }}
-            />
-
-            <Stack.Screen
+            <Drawer.Screen
               name="Home"
               component={HomeScreen}
               options={{ headerShown: true, title: 'Inicio' }}
             />
 
-            <Stack.Screen
-              name="TakePicture"
-              component={TakePictureStepScreen}
-              options={({ route }) => ({ title: 'Tomar foto' })}
-            />
-
-            <Stack.Screen
-              name="CameraTest"
-              component={CameraTestScreen}
-              options={({ route }) => ({ title: 'Prueba de cámara' })}
-            />
-
-            <Stack.Screen
+            <Drawer.Screen
               name="ApprovedSubjects"
               component={ApprovedSubjects}
               options={{
                 headerShown: true,
                 title: 'Materias aprobadas',
-                headerRight: () => <FilterNavBarButton/>,
+                headerRight: () => <FilterNavBarButton />,
               }}
             />
 
-            <Stack.Screen
+            <Drawer.Screen
               name="PendingSubjects"
               component={PendingSubjects}
-              options={{ 
-                headerShown: true, 
-                title: 'Materias pendientes', 
-                headerRight: () => <FilterNavBarButton/>,
+              options={{
+                headerShown: true,
+                title: 'Materias pendientes',
+                headerRight: () => <FilterNavBarButton />,
               }}
             />
 
-            <Stack.Screen
+            <Drawer.Screen
               name="DeliverFinalExam"
               component={DeliverFinalExam}
               options={{ headerShown: true, title: 'Entregar examen final' }}
             />
 
-            <Stack.Screen
+            <Drawer.Screen
               name="VerifyIdentity"
               component={VerifyIdentity}
               options={{ headerShown: true, title: 'Verificar identidad' }}
             />
 
-            <Stack.Screen
+            <Drawer.Screen
+              name="Splash"
+              component={SplashScreen}
+              options={{ headerShown: false }}
+            />
+
+            <Drawer.Screen
+              name="Landing"
+              component={LandingScreen}
+              options={{ headerShown: false, title: 'Inicio' }}
+            />
+
+            <Drawer.Screen
+              name="PreRegister"
+              component={PreRegisterScreen}
+              options={{ headerShown: true, title: 'Pre-registro' }}
+            />
+
+            <Drawer.Screen
               name="PreRegisterDone"
               component={PreRegisterLastInstructionsScreen}
               options={{ headerShown: false }}
             />
 
+            {/* <Drawer.Screen
+              name="TakePicture"
+              component={TakePictureStepScreen}
+              options={({ route }) => ({ title: 'Tomar foto' })}
+            /> */}
+
+            {/* <Drawer.Screen
+              name="CameraTest"
+              component={CameraTestScreen}
+              options={({ route }) => ({ title: 'Prueba de cámara' })}
+            /> */}
+
             {/* 
-            <Stack.Screen
+            <Drawer.Screen
               name="Historial de una materia"
               component={SubjectHistoryScreen}
               options={({ route }) => ({
@@ -105,7 +130,7 @@ const App = () => {
                 headerShown: true,
               })}
             /> */}
-          </Stack.Navigator>
+          </Drawer.Navigator>
         </NavigationContainer>
       </ActionSheetProvider>
     </Provider>
--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -11,145 +11,18 @@ Icon.loadFont();
 
 
 const Home: React.FC<any> = () => {
-  const [menuOpened, setMenuOpened] = useState(false);
-  const menuOpenerAnimator = useState(new Animated.ValueXY({ x: 0, y: 0 }))[0];
-  const radialMenuLayoutRef = useRef(null);
-  const menuOpenerLayoutRef = useRef(null);
-  const navigation = useNavigation();
-
-  const openMenu = () => {
-    if (radialMenuLayoutRef.current) { // check if radialMenuLayoutRef.current exists
-      const { x, y, width, height } = radialMenuLayoutRef.current;
-      const menuOpenerLayout: { width: number, height: number, x: number, y: number } = menuOpenerLayoutRef.current || { width: 0, height: 0, x: 0, y: 0 };
-
-      if (menuOpenerLayout) {
-        const expectedX = x + width / 2 - menuOpenerLayout.width / 2;
-        const expectedY = y + height / 2 - menuOpenerLayout.height / 2;
-
-        Animated.timing(menuOpenerAnimator, {
-          toValue: { x: expectedX - menuOpenerLayout.x, y: expectedY - menuOpenerLayout.y },
-          duration: 200,
-          easing: Easing.quad, // use Easing.quad
-          useNativeDriver: true, // add useNativeDriver property
-        }).start(() => {
-          setMenuOpened(true);
-        });
-      }
-    }
-  };
-
-  const closeMenu = () => {
-    setMenuOpened(false);
-    Animated.timing(menuOpenerAnimator, {
-      toValue: { x: 0, y: 0 },
-      duration: 200,
-      easing: Easing.quad,
-      useNativeDriver: false
-    }).start();
-  };
-
-  const handleLayout = (event: LayoutChangeEvent, ref: React.MutableRefObject<any>) => {
-    ref.current = event.nativeEvent.layout;
-  };
 
   return (
     <View style={style().view}>
-      {!menuOpened && (
+      {/* {!menuOpened && (
         <View
           onLayout={(event) => handleLayout(event, radialMenuLayoutRef)}
           style={style().menu}
         />
-      )}
+      )} */}
       <View style={style().mainView}>
         <Text style={style().text}>Bienvenido al Inicio</Text>
       </View>
-      {menuOpened && (
-        <RadialMenu
-          // onLayout={(event: any) => handleLayout(event, radialMenuLayoutRef)}
-          opened
-          onClosed={() => {
-            closeMenu();
-          }}
-          style={style().menu}
-        >
-          <TouchableOpacity style={style().menuRootImageItem}>
-            
-            <Image
-              style={style().itemImage}
-              source={require('./img/logo_fiuba.png')}
-            />
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={style().menuItem}
-            onPress={() => {
-              navigation.navigate('VerifyIdentity')
-            }}>
-            <Icon style={style().itemIcon} name="face-recognition"/>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={style().menuItem}
-            onPress={() => {
-              navigation.navigate('DeliverFinalExam')
-            }}>
-            <Icon style={style().itemIcon} name="qrcode-scan"/>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={style().menuItem}
-            onPress={() => {
-              navigation.navigate('PendingSubjects')
-            }}>
-            <Icon style={style().itemIcon} name="file-question"/>
-
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={style().menuItem}
-            onPress={() => {
-              navigation.navigate('ApprovedSubjects')
-            }}>
-            <Icon style={style().itemIcon} name="file-check"/>
-
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={style().menuItem}
-            onPress={async () => {
-              await SessionManager.getInstance()?.clearCredentials();
-              navigation.reset({
-                index: 0,
-                routes: [{ name: 'Landing' }],
-              });
-            }}>
-            <Icon style={style().itemIcon} name="logout"/>
-          </TouchableOpacity>
-        </RadialMenu>
-      )}
-      {!menuOpened && (
-        <Animated.View
-          style={[
-            style().menuOpenerContainer,
-            {
-              transform: menuOpenerAnimator.getTranslateTransform(),
-            },
-          ]}
-          onLayout={(event) => handleLayout(event, menuOpenerLayoutRef)}
-        >
-          <TouchableHighlight
-            style={style().menuImageOpener}
-            onPress={() => {
-              openMenu();
-            }}
-          >
-            <Image
-              style={style().itemImage}
-              source={require('./img/logo_fiuba.png')}
-            />
-          </TouchableHighlight>
-        </Animated.View>
-      )}
     </View>
   );
 };
