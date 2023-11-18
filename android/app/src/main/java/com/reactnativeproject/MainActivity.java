653d9c2921a363fd9be2c14b05ab660a83b0fd2d
franco-giordano <31712363+franco-giordano@users.noreply.github.com>
2023-11-18 13:32:16 -0300

    Change app package name to org.erinc.ludo2

--- android/app/src/main/java/com/reactnativeproject/MainActivity.java
+++ android/app/src/main/java/com/reactnativeproject/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativeproject;
+package org.erinc.ludo2;
 import expo.modules.ReactActivityDelegateWrapper;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
