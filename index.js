d81439fdf423a2f0e344c1129f3bdf2b9d82c0a4
franco-giordano <31712363+franco-giordano@users.noreply.github.com>
2024-06-22 02:24:16 -0300

    Silence react navigation warning

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ import App from './App';
 import { name as appName } from './app.json';
 
 import { LogBox } from 'react-native';
-LogBox.ignoreLogs(['new NativeEventEmitter']); // To fix this we should upgrade to react-navigation 6.X AFAIK
+LogBox.ignoreLogs([
+    'new NativeEventEmitter', // To fix this we should upgrade to react-navigation 6.X AFAIK
+    'TypeError: _reactNative.Dimensions.removeEventListener is not a function (it is undefined)' // To fix this we should... probably also upgrade react-navigation
+]);
 
 import moment from "moment";
 import "moment/locale/es"
