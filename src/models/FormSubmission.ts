4d8e6deab8e60f8e1deac9401048d72282e702db
mgonzalezp <mgonzalezp@fi.uba.ar>
2026-04-26 02:29:01 -0300

    feat: add user information fields to FormSubmission model

--- src/models/FormSubmission.ts
+++ src/models/FormSubmission.ts
@@ -6,6 +6,11 @@ export interface FormAnswer {
 
 export default interface FormSubmission {
   submission_id: number;
+  user_id: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  role: string;
   student_first_name: string;
   student_last_name: string;
   student_padron: string | null;
